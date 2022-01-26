0682bd46e82fa163953ce2e823e0ec55e25aeb19
Kaushik Bishnu <kaushik.bishnu@gmail.com>
2022-01-26 22:22:11 +0100

    test

--- provider.tf
+++ provider.tf
@@ -1,22 +1,13 @@
-#starter
+# The following variable is used to configure the provider's authentication
+# token. You don't need to provide a token on the command line to apply changes,
+# though: using the remote backend, Terraform will execute remotely in Terraform
+# Cloud where your token is already securely stored in your workspace!
+
+variable "provider_token" {
+  type = string
+  sensitive = true
+}
+
 provider "fakewebservices" {
   token = var.provider_token
 }
-
-# provider "azurerm" {
-#   /*subscription_id = var.azure.subscription_id
-#   client_id       = var.azure.client_id
-#   client_secret   = var.azure.client_secret
-#   tenant_id       = var.azure.tenant_id*/
-
-#   # use latest
-#   ##version         = 2.1
-
-#   features {}
-# }
-
-
-
-
-
-
