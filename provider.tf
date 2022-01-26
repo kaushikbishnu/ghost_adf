da131f672342adaeb064a995ae6d1a94e231bae2
Kaushik Bishnu <kaushik.bishnu@gmail.com>
2022-01-26 17:43:50 +0100

    adding provider and variables

---
+++ provider.tf
@@ -0,0 +1,11 @@
+provider "azurerm" {
+  /*subscription_id = var.azure.subscription_id
+  client_id       = var.azure.client_id
+  client_secret   = var.azure.client_secret
+  tenant_id       = var.azure.tenant_id*/
+
+  # use latest
+  ##version         = 2.1
+
+  features {}
+}
---
+++ variables.tf
@@ -0,0 +1,11 @@
+# azure service principal info
+variable azure {
+  default = {
+    subscription_id = "_add_here_"
+    # client_id or app_id
+    client_id       = "_add_here_"
+    client_secret   = "_add_here_"
+    # tenant_id or directory_id
+    tenant_id       = "_add_here_"
+  }
+}
